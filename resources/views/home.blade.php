a6e1157fbc5d68e27ac4110c03da138788d3d098
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-05-12 10:19:41 +0200

    aggiunta stile grafico delle cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app')
 @section('content')
 <h2 class="text-center">My Movies</h2>
-<div class="container d-flex wrap">
+<div class="container d-flex flex-wrap">
 
 @foreach($movies as $movie)
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
+<div class="card m-1" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="{{$movie->title }}">
   <div class="card-body">
-    <h5 class="card-title">{{$movie->title }}</h5>
-    <p class="card-text">{{$movie->original_title }}</p>
-    <p class="card-text">{{$movie->nationality }}</p>
-    <p class="card-text">{{$movie->vote }}</p>
+    <h5 class="card-title">Title: {{$movie->title }}</h5>
+    <p class="card-text">Original Title:{{$movie->original_title }}</p>
+    <p class="card-text">Nationality: {{$movie->nationality }}</p>
+    <p class="card-text">IMdB Vote: {{$movie->vote }}</p>
 
   </div>
 </div>
